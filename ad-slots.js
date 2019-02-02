e80fa3d83a50be39e5df2b0d3c9f515d707b1c54
Michael Ajah <luigiajah@gmail.com>
2019-02-02 14:59:37 +0100

    Update ad-slots.js

--- ad-slots.js
+++ ad-slots.js
@@ -1,30 +1,30 @@
 
  ads = new Array(9);
     ads[0] = "<a href='https://goo.gl/FzaGhd' rel='nofollow' target='blank'>" + 
-             "<img height='320px' src='https://naijatechguy.github.io/symac23.png'/></a>";
+             "<img height='320px' src='https://i.imgur.com/tJKxLN0.png'/></a>";
 
-    ads[1] = "<a href='https://goo.gl/UyJNbX' rel="'nofollow' target="'blank'">" +
-             "<img height='320px' src='https://naijatechguy.github.io/2ARyCfZ.png'/></a>";
+    ads[1] = "<a href='https://goo.gl/UyJNbX' rel='nofollow' target='blank'>" +
+             "<img height='320px' src='https://i.imgur.com/2ARyCfZ.png'/></a>";
 
     ads[2] = "<a href='https://goo.gl/FzaGhd' rel='nofollow' target='blank'>" + 
-             "<img height='320px' src='https://naijatechguy.github.io/symac23.png'/></a>";
+             "<img height='320px' src='https://i.imgur.com/tJKxLN0.png'/></a>";
 
     ads[3] = "<a href='https://goo.gl/UyJNbX' rel='nofollow' target='blank'>" +
-             "<img height='320px' src='https://naijatechguy.github.io/2ARyCfZ.png'/></a>";
+             "<img height='320px' src='https://i.imgur.com/2ARyCfZ.png'/></a>";
 
  ads[4] = "<a href='https://goo.gl/MN6ocq' rel='nofollow' target='blank'>" +
-             "<img height='320px' src='/wp-content/uploads/2019/01/1.jpg'/></a>";
+             "<img height='320px' src='https://i.imgur.com/0UWQqhN.png.jpg'/></a>";
 
     ads[5] = "<a href='https://goo.gl/FzaGhd' rel='nofollow' target='blank'>" + 
-             "<img height='320px' src='https://naijatechguy.github.io/symac23.png'/></a>";
+             "<img height='320px' src='https://i.imgur.com/tJKxLN0.png'/></a>";
 
 ads[6] = "<a href='https://goo.gl/FzaGhd' rel='nofollow' target='blank'>" + 
-             "<img height='320px' src='https://naijatechguy.github.io/symac23.png'/></a>";
+             "<img height='320px' src='https://i.imgur.com/tJKxLN0.png'/></a>";
 
 ads[7] = "<a href='https://goo.gl/FzaGhd' rel='nofollow' target='blank'>" + 
-             "<img height='320px' src='https://naijatechguy.github.io/symac23.png'/></a>";
+             "<img height='320px' src='https://i.imgur.com/d6juJ8O.jpg'/></a>";
 
 ads[8] = "<a href='https://goo.gl/FzaGhd' rel='nofollow' target='blank'>" + 
-             "<img height='320px' src='https://naijatechguy.github.io/symac23.png'/></a>";
+             "<img height='320px' src='https://i.imgur.com/d6juJ8O.jpg'/></a>";
     index = Math.floor(Math.random() * ads.length);
     document.write(ads[index]); 
