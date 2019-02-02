92f99ce255d2818757063511b2522f7cc7a9003c
Michael Ajah <luigiajah@gmail.com>
2019-02-02 14:41:37 +0100

    Create slots.js

---
+++ slots.js
@@ -0,0 +1,5 @@
+ ads = new Array(0);
+    ads[0] = "<a href='https://goo.gl/FzaGhd' rel='nofollow' target='blank'>" + 
+             "<img height='320px' src='https://i.imgur.com/tJKxLN0.png'/></a>";
+    index = Math.floor(Math.random() * ads.length);
+    document.write(ads[index]); 
